ee52aa6a55b5e6d3991b0799d042496bd03d3862
Syhnn <jeremy.lobreaux@gmail.com>
2020-09-22 16:13:34 +0200

    Added advancement on stage 4-2

---
+++ Level 4/4-2/level 4-2 mirrors.py
@@ -0,0 +1,73 @@
+﻿import math
+
+def obstacleCheck(list, target, fact):
+  EPS = 0.0000000001
+  for obs in list:
+    if (obs == target or
+        not 0 <= obs[0] <= target[0] and not 0 >= obs[0] >= target[0] or
+        not 0 <= obs[1] <= target[1] and not 0 >= obs[1] >= target[1]):
+      continue
+    if -EPS < fact * obs[0] - obs[1] < EPS:
+      return False
+  return True
+
+def solution(dimensions, your_position, guard_position, distance):
+  player_x = your_position[0]
+  player_y = your_position[1]
+  guard_x = guard_position[0]
+  guard_y = guard_position[1]
+
+  map_width = dimensions[0]
+  map_height = dimensions[1]
+  max_distance = distance
+  hit_count = 0
+  vertical_case = False
+  
+  mirror_players = []
+  mirror_guards = []
+  mirror_corners = []
+
+  distance = math.sqrt(math.pow(guard_x - player_x, 2) + math.pow(guard_y - player_y, 2))
+
+  if distance > max_distance:
+    return 0
+  elif distance == max_distance:
+    return 1
+
+  mirror_guards.append([guard_x - player_x, guard_y - player_y])
+  
+  i_limit = max_distance / map_width + 1
+  j_limit = max_distance / map_height + 1
+
+  for x in range(- i_limit, i_limit + 1):
+    for y in range(- j_limit, j_limit + 1):
+      if not x == - i_limit and not y == - j_limit:
+        mirror_corners.append([x * map_width - player_x, y * map_height - player_y])
+
+      if x == y == 0:
+        continue
+        
+      mirror_players.append([(x / 2 * player_x * 2) + ((x + 1) / 2 * (map_width - player_x) * 2),
+                             (y / 2 * player_y * 2) + ((y + 1) / 2 * (map_height - player_y) * 2)])
+      mirror_guards.append([guard_x - player_x + (x / 2 * guard_x * 2) + ((x + 1) / 2 * (map_width - guard_x ) * 2),
+                            guard_y - player_y + (y / 2 * guard_y * 2) + ((y + 1) / 2 * (map_height - guard_y) * 2)])
+
+  for guard in mirror_guards:
+    distance = math.sqrt(math.pow(guard[0], 2) + math.pow(guard[1], 2))
+    if distance < max_distance:
+      try:
+        a = float((guard[1])) / float((guard[0]))
+      except ZeroDivisionError:
+        if not vertical_case:
+          hit_count = hit_count + 1
+          vertical_case = True
+        continue
+      if (not obstacleCheck(mirror_guards, guard, a) or
+          not obstacleCheck(mirror_players, guard, a) or
+          not obstacleCheck(mirror_corners, guard, a)):
+        continue
+      hit_count = hit_count + 1
+
+  return hit_count
+
+print solution([2,3], [1,1], [2,1], 200)
--- Level 4/4-2/level 4-2.py
+++ Level 4/4-2/level 4-2.py
@@ -1,11 +1,85 @@
 import math
 
 class solver:
-  def hitCheck(self, angle):
+  def gcd(self, a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+  def reduce_frac(self, n, d):
+
+    if d == 0:
+      return None
+
+    cd = self.gcd(n, d)
+    n, d = n / cd, d / cd
+
+    return n, d
+
+  def hitCheck(self, vector):
     current_pos_x = self.player_x
     current_pos_y = self.player_y
+    direction_x = self.player_x + vector[0]
+    direction_y = self.player_y + vector[1]
+    remaining_range = self.max_distance
+    EPS = 0.00000000001
 
-    return False
+    # shooting vertically
+    if vector[0] == 0:
+      return self.player_x == self.guard_x and (self.guard_y > self.player_y and vector[1] > 0 or self.guard_y < self.player_y and vector[1] < 0)
+    # shooting horizontally
+    elif vector[1] == 0:
+      return self.player_y == self.guard_y and (self.guard_x > self.player_x and vector[0] > 0 or self.guard_x < self.player_x and vector[0] < 0)
+    else:
+      # calculate direction equation
+      a = float((current_pos_y - direction_y)) / float((current_pos_x - direction_x))
+      b = current_pos_y - a * current_pos_x
+
+      while True:
+        if -EPS < a * self.guard_x + b - self.guard_y < EPS:
+          # guard is on the direction line
+          # check return true if direction is good and distance is good
+          # otherwise continue
+          if (self.guard_y > current_pos_y and vector[1] > 0 or self.guard_y < current_pos_y and vector[1] < 0) and (self.guard_x > current_pos_x and vector[0] > 0 or self.guard_x < current_pos_x and vector[0] < 0):
+            d = math.sqrt(math.pow(current_pos_y - self.guard_y, 2) + math.pow(current_pos_x - self.guard_x, 2))
+            return d < remaining_range
+        
+        x_limit = 0
+        if vector[0] > 0:
+          x_limit = self.map_width
+
+        y_limit = 0
+        if vector[1] > 0:
+          y_limit = self.map_height
+
+        y = a * x_limit + b
+        
+        if y == y_limit:
+          # corner hit
+          return False
+        elif not 0 < y < self.map_height:
+          vector[1] = -vector[1]
+          newx = float(y_limit - b) / a
+          d = math.sqrt(math.pow(current_pos_y - y_limit, 2) + math.pow(current_pos_x - newx, 2))
+          if d < remaining_range:
+            remaining_range = remaining_range - d
+            current_pos_y = y_limit
+            current_pos_x = newx
+          else:
+            return False
+        else:
+          vector[0] = -vector[0]
+          newy = a * x_limit + b
+          d = math.sqrt(math.pow(current_pos_y - newy, 2) + math.pow(current_pos_x - x_limit, 2))
+          if d < remaining_range:
+            remaining_range = remaining_range - d
+            current_pos_y = newy
+            current_pos_x = x_limit
+          else:
+            return False
+
+        a = -a
+        b = current_pos_y - a * current_pos_x
 
   def solve(self, dimensions, your_position, guard_position, distance):
     self.player_x = your_position[0]
@@ -22,15 +96,39 @@ class solver:
     elif self.distance == self.max_distance:
       return 1
 
-    trajectories = 1
+    trajectories = 0
 
-    if hitCheck():
-      trajectories = trajectories + 1
+    test = 100
+    directions = []
+    for x in range(1, test):
+      for y in range(1, test):
+        if x == 0 or y == 0:
+          continue
+
+        if not x&1 and not y&1:
+          continue
+
+        a, b = self.reduce_frac(x, y)
+
+        if not [a, b] in directions:
+          directions.append([a, b])
+          directions.append([a, -b])
+          directions.append([-a, b])
+          directions.append([-a, -b])
+          
+    directions.append([ 1,  0])
+    directions.append([-1,  0])
+    directions.append([ 0,  1])
+    directions.append([ 0, -1])
+
+    for dir in directions:
+      if self.hitCheck(dir):
+        trajectories = trajectories + 1
 
     return trajectories
 
 def solution(dimensions, your_position, guard_position, distance):
   s = solver()
-  s.solve(dimensions, your_position, guard_position, distance)
+  return s.solve(dimensions, your_position, guard_position, distance)
 
 print solution([3,2], [1,1], [2,1], 4)
