67dee94c173ba77258f367cae5132ba9cec3db33
Syhnn <jeremy.lobreaux@gmail.com>
2020-09-28 12:00:28 +0200

    Found an even better solution to 2-2

--- Level 2/2-2/level 2-2.py
+++ Level 2/2-2/level 2-2.py
@@ -11,9 +11,15 @@ def solution(x, y):
 def better_solution(x, y):
   return x * (x + 1) / 2 + (x + y - 2) * (x + y - 1) / 2 - x * (x - 1) / 2
 
+def better_solution2(x, y):
+  return x + (x + y - 2) * (x + y - 1) / 2
+
 start = timeit.default_timer()
 solution(10000000, 10000000)
-print timeit.default_timer() - start # around 2.25s, complexity would be... O(n²) ?
+print timeit.default_timer() - start # around 2.25s, complexity would be... O(n^2) ?
 start = timeit.default_timer()
 better_solution(1000000000000000000000000000000000000, 10000000000000000000000000000000000000)
 print timeit.default_timer() - start # around 1.4e-5, complexity would be O(1) i guess
+start = timeit.default_timer()
+better_solution2(1000000000000000000000000000000000000, 10000000000000000000000000000000000000)
+print timeit.default_timer() - start # even faster and less complicated... still O(1)
